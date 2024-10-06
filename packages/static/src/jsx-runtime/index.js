0c3d52a5daa408470937f8c32aa1f94ae290d4b8
Ivan Molina Rebolledo <ivan@ivmoreau.com>
2024-10-06 03:16:56 -0600

    stop using links

--- packages/static/src/jsx-runtime/index.js
+++ packages/static/src/jsx-runtime/index.js
@@ -1,10 +1,1 @@
-import { h } from "preact"
-import { Fragment as F } from "preact"
-
-export const jsx = (...p) => {
-    return h(p[0], p[1], ...(Array.isArray(p[1].children) ? p[1].children : [p[1].children]))
-}
-
-export const jsxDEV = jsx
-
-export const Fragment = F
+export * from "../runtime"
